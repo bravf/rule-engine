e43b61c82264c00e75396f7f4b2a5393585eeb30
木已成鲨鱼 <bravfing@126.com>
2024-10-23 09:08:08 +0800

    update: 更新代码

--- system.r
+++ system.r
@@ -1,28 +1,36 @@
 ##### 标准库
-
 sys = seq.map()
-sys.output = seq.map('is_hit',false, 'money',0 ,'log', '')
+
+## 输出对象，最终返回给java
+sys.out_data = seq.map('is_hit',false, 'money',0 ,'log', '')
+
+## 打印日志
 sys.log = fn(&logs) {
-  for _log in logs{
-    sys.output.log = sys.output.log + _log
+  for log in logs{
+    sys.out_data.log = sys.out_data.log + log + '\n'
   }
 }
-sys.add_output = fn(key,value) {
-  sys.output[key] = value
+
+## 增加输出项
+sys.output = fn(key,value) {
+  sys.out_data[key] = value
 }
+
+## 设置规则是否命中
 sys.hit = fn(is_hit) {
-  sys.output.is_hit = is_hit
+  sys.out_data.is_hit = is_hit
 }
+
+## 设置质控金额
 sys.set_money = fn(money) {
-  sys.output.money = money
-}
-sys.baohan = fn(baohan) {
-
+  sys.out_data.money = money
 }
 
-##case1
+## 
+
+## case1
 sys.log('nihao');
 sys.hit(true);
 sys.set_money(100);
-sys.add_output('test','test');
-return sys.output;
+sys.output('test','test');
+return sys.out_data;
