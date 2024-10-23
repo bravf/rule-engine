ccef19d7df250a9ba460f33de01f754e913e0b7f
木已成鲨鱼 <bravfing@126.com>
2024-10-23 10:19:03 +0800

    udpate: 更新代码

--- system.r
+++ system.r
@@ -1,80 +1,111 @@
+#### 数据注入
+in_data = {
+  base_info: {
+    yibaojiesuandengji: '',
+    nianling: '',
+    ....
+  },
+  zhenduan: {
+    zhenduanbianma_he_mingchegn: {
+      bianma:'',
+      mingchegn: ''
+    },
+    qita: [
+      {bianma:'',mingchegn:''},
+      {bianma:'',mingchegn:''},
+    ]
+  },
+}
+
+
 ##### 标准库
-sys = seq.map()
+sys = seq.map();
 
 ## 输出对象，最终返回给java
-sys.out_data = seq.map('is_hit',false, 'money',0 ,'log', '')
+sys.out_data = seq.map('is_hit',false, 'money',0 ,'log', '');
 
 ## 打印日志
 sys.log = fn(&logs) {
   for log in logs{
-    sys.out_data.log = sys.out_data.log + log + '\n'
+    sys.out_data.log = sys.out_data.log + log + '\n';
   }
-}
+};
 
 ## 增加输出项
 sys.output = fn(key,value) {
-  sys.out_data[key] = value
+  sys.out_data[key] = value;
+};
+
+
+## 从下表模块取数
+sys.get_data_from_xiabiao = fn(&code){
+
+}
+
+## 从数据库取数
+sys.get_data_from_sql = fn(sql){
+  
 }
 
 ## 设置规则是否命中
 sys.hit = fn(is_hit) {
-  sys.out_data.is_hit = is_hit
-}
+  sys.out_data.is_hit = is_hit;
+};
 
 ## 设置质控金额
 sys.set_money = fn(money) {
-  sys.out_data.money = money
-}
+  sys.out_data.money = money;
+};
 
 ## 含有当前码段且含有名称
 sys.hanyou = fn(){
 
-}
+};
 
 ## 含有当前码段且不含有名称
 sys.hanyou2 = fn(){
 
-}
+};
 
 ## 多个编码中含有两个及以上不同类目
 sys.hanyou3 = fn(){
 
-}
+};
 
 ## 多个编码中含有两个及以上亚目
 sys.hanyou4 = fn(){
   
-}
+};
 
 ## 含有两个及以上
 sys.hanyou5 = fn(){
 
-}
+};
 
 ## 全部含有
 sys.hanyou6 = fn(){
-  
-}
+
+};
 
 ## 不含有当前码段但含有名称
 sys.buhanyou = fn(){
 
-}
+};
 
 ## 不含有当前码段且不含有名称
 sys.buhanyou2 = fn(){
 
-}
+};
 
 ## 代码精确含有且名称模糊含有
 sys.jingquehanyou = fn(){
 
-}
+};
 
 ## 代码精确不含有且名称模糊含有
 sys.jingquebuhanyou = fn(){
 
-}
+};
 
 
 
