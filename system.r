7615190e984871618fae7999546787523b44bc7e
木已成鲨鱼 <bravfing@126.com>
2024-10-23 09:19:32 +0800

    update: 更新标准库

--- system.r
+++ system.r
@@ -26,11 +26,64 @@ sys.set_money = fn(money) {
   sys.out_data.money = money
 }
 
-## 
+## 含有当前码段且含有名称
+sys.hanyou = fn(){
+
+}
+
+## 含有当前码段且不含有名称
+sys.hanyou2 = fn(){
+
+}
+
+## 多个编码中含有两个及以上不同类目
+sys.hanyou3 = fn(){
+
+}
+
+## 多个编码中含有两个及以上亚目
+sys.hanyou4 = fn(){
+  
+}
+
+## 含有两个及以上
+sys.hanyou5 = fn(){
+
+}
+
+## 全部含有
+sys.hanyou6 = fn(){
+  
+}
+
+## 不含有当前码段但含有名称
+sys.buhanyou = fn(){
+
+}
+
+## 不含有当前码段且不含有名称
+sys.buhanyou2 = fn(){
+
+}
+
+## 代码精确含有且名称模糊含有
+sys.jingquehanyou = fn(){
+
+}
+
+## 代码精确不含有且名称模糊含有
+sys.jingquebuhanyou = fn(){
+
+}
+
+
+
+
+
 
 ## case1
-sys.log('nihao');
-sys.hit(true);
-sys.set_money(100);
-sys.output('test','test');
-return sys.out_data;
+## sys.log('nihao');
+## sys.hit(true);
+## sys.set_money(100);
+## sys.output('test','test');
+## return sys.out_data;
